5e2a744696492d09aa0b707862c6fe2c4491c368
AbdooOwd <90041629+AbdooOwd@users.noreply.github.com>
2024-03-09 13:56:34 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Home = () => {
+  const [generatedText, setGeneratedText] = useState("");
+  const [prompt, setPrompt] = useState(""); // State to store user input
+
+  const generateText = async () => {
+    try {
+      setGeneratedText("Typing...");
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }), // Send user input as JSON in the request body
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setGeneratedText(data.text);
+      } else {
+        setGeneratedText(`Error: ${data.error}`);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setGeneratedText("An error occured!");
+    }
+  };
+
+  return (
+    <div>
+      <div className="whole">
+        <h1>Cirine AI</h1>
+        <div className="generated-text">{generatedText}</div>
+
+        <div id="chatting">
+          <input
+            type="text"
+            id="prompt"
+            name="prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Chat..."
+            required
+          />
+          <button onClick={generateText}>Generate Text</button>
+        </div>
+        <p style={{ fontSize: "0.6rem" }}>By AbdooOwd</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
