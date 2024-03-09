4d2142a0cfd8a5ce9a9ac08ec3b18f83f9170d5d
AbdooOwd <90041629+AbdooOwd@users.noreply.github.com>
2024-03-09 17:40:15 +0000

    clarification with comments

--- pages/api/generate.tsx
+++ pages/api/generate.tsx
@@ -10,15 +10,16 @@ const handler = async (req, res) => {
     const { prompt } = req.body;
 
     const output = await replicate.run(
+      // This model is cool :ok_hand:
       "lucataco/dolphin-2.2.1-mistral-7b:0521a0090543fea1a687a871870e8f475d6581a3e6e284e32a2579cfb4433ecf",
       {
         input: {
           top_k: 50,
           top_p: 0.95,
-          prompt: prompt,
+          prompt: prompt, // The user's prompt
           temperature: 0.8,
           max_new_tokens: 1024,
-          prompt_template: base_prompt,
+          prompt_template: base_prompt, // The... AI's brain?
           presence_penalty: 0,
           frequency_penalty: 0,
         },
@@ -26,7 +27,7 @@ const handler = async (req, res) => {
     );
 
     if (Array.isArray(output) && output.length > 0) {
-      const fullText = output.join(""); // Concatenate array elements into a single string
+      const fullText = output.join(""); // Put thing into one string
       return res.status(200).json({ text: fullText });
     } else {
       return res.status(200).json({ error: "No choices available" });
